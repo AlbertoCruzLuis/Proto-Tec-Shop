25ae3bfae91a6bc7f18131770a1c3ab789b65e3d
Alberto Cruz <albertocruzluis9@gmail.com>
2020-02-02 23:52:36 +0000

    Subtask 8: Create Class ShoppingCart

---
+++ ShoppingCart.cpp
@@ -0,0 +1,33 @@
+#include "ShoppingCart.h"
+
+ShoppingCart::ShoppingCart()
+{
+    _sumNetPrice = 0;
+    _sumGrossPrice = 0;
+    _minFreeShipping = 1000;
+}
+
+ShoppingCart::~ShoppingCart()
+{
+    std::cout << "~ShoppingCart()";
+    delete _articles;
+    std::cout << "delete article: "; 
+}
+
+void ShoppingCart::add_article(int amount, Article* article)
+{
+    _articles = new InCart();
+
+    //Actualising the two sum variables
+    _sumNetPrice += article->get_netPrice;
+    _sumGrossPrice += article->get_grossPrice;
+}
+
+virtual void ShoppingCart::print()
+{
+    std::cout << _sumNetPrice << std::endl;
+    std::cout << _sumGrossPrice << std::endl;
+
+    //Shipping Cost 5.90 EUR
+    std::cout << "shipping: 5.90 EUR\n";
+}
---
+++ ShoppingCart.h
@@ -0,0 +1,28 @@
+#ifndef SHOPPING_CART_H
+#define SHOPPING_CART_H
+
+#include "InCart.h"
+
+class ShoppingCart
+{
+    InCart* _articles;
+    int _minFreeShipping;
+    double _sumNetPrice;
+    double _sumGrossPrice;
+
+    public:
+
+        ShoppingCart();
+        ~ShoppingCart();
+
+        //Setters
+        void set_minFreeShipping(int minFreeShipping)
+        { _minFreeShipping = minFreeShipping; }
+
+
+        void add_article(int, Article*);
+        virtual void print();
+
+};
+
+#endif
